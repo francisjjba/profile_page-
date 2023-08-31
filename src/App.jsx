21fa099813fe38730979107c9b59a10a325b62e3
francisjjba <francisjjba@gmail.com>
2023-08-31 13:28:30 +0530

    31-08-2023  did some changes added some button

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className='heading'>
       <h1 className="title-name">Francis Dsouza</h1>
-       <img src="" alt="" />
+       <img className="profile_image" src="https://raw.githubusercontent.com/francisjjba/profile_page-/main/src/assets/img.jpg" alt="https://raw.githubusercontent.com/francisjjba/profile_page-/main/src/assets/img.jpg" />
+       
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+       <a href="https://www.linkedin.com/in/francis-dsouza-9785261ba/" class="fa fa-linkedin"></a>
+       <a href="mailto:francis97412@gmail.com" class="fa fa-google"></a>
+
     </div>
   )
 }
