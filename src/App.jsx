6c3f3be963e3438e6788b31b3488fcc831886f34
francisjjba <francisjjba@gmail.com>
2023-08-31 12:58:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+
+function App() {
+  return (
+    <div className='heading'>
+      <h1 className="title-name">Francis Dsouza</h1>
+       <img src="" alt="" />
+    </div>
+  )
+}
+
+export default App
