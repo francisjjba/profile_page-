eb0e59937f04cc8a36aa92d0e03de2f33af01c65
francisjjba <francisjjba@gmail.com>
2023-09-05 11:08:58 +0530

    added few button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 
 
+
 function App() {
   return (
     <div className='heading'>
@@ -8,8 +9,12 @@ function App() {
        <img className="profile_image" src="https://raw.githubusercontent.com/francisjjba/profile_page-/main/src/assets/img.jpg" alt="https://raw.githubusercontent.com/francisjjba/profile_page-/main/src/assets/img.jpg" />
        
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-       <a href="https://www.linkedin.com/in/francis-dsouza-9785261ba/" class="fa fa-linkedin"></a>
-       <a href="mailto:francis97412@gmail.com" class="fa fa-google"></a>
+       <a href="https://www.linkedin.com/in/francis-dsouza-9785261ba/" className="fa fa-linkedin"></a>
+       <a href='francis97412@gmail.com' className="fa fa-google"/>
+       <a href="https://github.com/francisjjba" className="fa fa-github" ></a>
+
+       
+
 
     </div>
   )
