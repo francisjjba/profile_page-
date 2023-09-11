7542b11cb30af9eca9ade1ae16226b08fabacb9b
francisjjba <francisjjba@gmail.com>
2023-09-11 12:21:33 +0530

    added some features and css

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,17 @@ function App() {
     <div className='heading'>
       <h1 className="title-name">Francis Dsouza</h1>
        <img className="profile_image" src="https://raw.githubusercontent.com/francisjjba/profile_page-/main/src/assets/img.jpg" alt="https://raw.githubusercontent.com/francisjjba/profile_page-/main/src/assets/img.jpg" />
+
+       <br />
        
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+       <div className="info-buttons">
        <a href="https://www.linkedin.com/in/francis-dsouza-9785261ba/" className="fa fa-linkedin"></a>
        <a href='francis97412@gmail.com' className="fa fa-google"/>
        <a href="https://github.com/francisjjba" className="fa fa-github" ></a>
-
+       <a href="https://github.com/francisjjba/profile_page-/blob/eb0e59937f04cc8a36aa92d0e03de2f33af01c65/src/assets/francis_resume.pdf" className="fa fa-file-pdf-o"></a>
+       </div>
        
 
 
